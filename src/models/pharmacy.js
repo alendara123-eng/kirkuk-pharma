e9dd641fc8d5b30d9e7fd0bd4afae56c5954d427
alendara123-eng <alendara123@gmail.com>
2025-09-04 16:22:14 +0300

    add pharmacy model

---
+++ src/models/pharmacy.js
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "./index.js";
+
+export const Pharmacy = sequelize.define("Pharmacy", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
